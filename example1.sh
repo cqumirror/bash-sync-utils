b843b9fcff2a9a5fe0961371cb4eea0f4335051e
Hagb <hagb@hagb.name>
2023-10-23 02:30:35 +0800

    Initial commit

---
+++ example1.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+#. /path/to/sync-utils.sh || {
+#    echo Failed to load sync-utils.sh >&2
+#    exit 1
+#}
+. "$(dirname -- "$0")"/sync-utils.sh || {
+    echo Failed to load sync-utils.sh >&2
+    exit 1
+}
+
+URL=https://github.com/rust-lang/crates.io-index.git
+#DST=/path/to/sync/dir/
+DST="$(dirname -- "$0")"/crates.io-index.git
+mkdir -p "$DST" 
+cd "$DST" || {
+    echo Failed to cd "$DST" >&2
+    exit 1
+}
+
+repo_init() {
+    run-sync-job git clone --mirror "$URL" "$DST"
+}
+
+update_git() {
+    # The following two job is interruptible. For example, killed by tunasync when
+    # `tunasync stop job-name` is run by user manually.
+    run-sync-job timeout 3600 git remote -v update
+    ret=$?
+    [ $ret != 0 ] && echo "git sync failed, status code $ret." >&2
+    # Assume that we want to run this cleanup even if sync failed.
+    ## If someone want to stop sync, just don't do the long-time cleanup!
+    for s in $pendding_signals $handled_signals; do
+        ## Check whether it is killed.
+        ## Notice that if we didn't use `timeout` command, we could check the
+        ## value of status code, which should be "128 + signal number" when
+        ## the process was killed by the signal. There are also some programs
+        ## returning different code: for example, rsync returns 20 after
+        ## killed by INT/TERM.
+        if [ $s = SIGTERM -o $s = SIGINT ]; then
+            echo "killed. don't do git repack." >&2
+            return $ret
+        fi
+    done
+    echo "git repack" >&2
+    run-sync-job git repack -a -b -d || echo "git repack failed!" >&2
+    return $ret
+}
+# Here is a more simple version if we do cleanup only after rync is successful:
+#update_git() {
+#    run-sync-job timeout 3600 git remote -v update &&
+#    run-sync-job git repack -a -b -d
+#}
+
+if [ ! -f "$DST/HEAD" ]; then
+    repo_init
+    exit $?
+fi
+
+update_git
---
+++ example2.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+#. /path/to/sync-utils.sh || {
+#    echo Failed to load sync-utils.sh >&2
+#    exit 1
+#}
+. "$(dirname -- "$0")"/sync-utils.sh || {
+    echo Failed to load sync-utils.sh >&2
+    exit 1
+}
+
+URL=rsync://example.com/example
+DST=/path/to/sync/dir
+TEMP_DIR=/path/to/temp/dir
+
+# The following one job is interruptible. For example, killed by tunasync when
+# `tunasync stop job-name` is run by user manually.
+run-sync-job rsync --temp-dir="$TEMP_DIR" some-other-args "$SRC" "$DST"
+
+# using the exit status to tell caller whether the job success is IMPORTANT!
+ret=$?
+
+# If rsync failed, it might leave some temporary files.
+if [ $ret != 0 ]; then
+    # with signals blocked, it should not run for too long.
+    rm -rf "$TEMP_DIR"
+    # If we are unsure that whether time needed by the cleanup is short enough,
+    # we can use the following one instead:
+    #timeout 1 rm -rf "$TEMP_DIR"
+fi
+
+# return the status of sync
+exit $ret
